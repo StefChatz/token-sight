dca1cf380a25981b0e8a04a04e1ff2b6b681ccdf
Stefanos Chatzakis <47855432+StefChatz@users.noreply.github.com>
2024-06-14 21:12:51 +0300

    fix failing build

--- config/index.ts
+++ config/index.ts
@@ -1,28 +1,28 @@
-import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
+import { defaultWagmiConfig } from '@web3modal/wagmi/react/config';
 
-import { cookieStorage, createStorage } from 'wagmi'
-import { mainnet, sepolia } from 'wagmi/chains'
+import { cookieStorage, createStorage } from 'wagmi';
+import { mainnet, sepolia } from 'wagmi/chains';
 
 // Get projectId from https://cloud.walletconnect.com
-export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
+export const projectId = '6a7fdfa8080206e492e4c0a976b5c931';
 
-if (!projectId) throw new Error('Project ID is not defined')
+if (!projectId) throw new Error('Project ID is not defined');
 
 const metadata = {
-  name: 'local',
-  description: 'local',
-  url: 'https://localhost:3000',
-  icons: ['https://avatars.githubusercontent.com/u/37784886']
-}
+  name: 'token-sight',
+  description: 'token-sight',
+  url: 'https://token-sight.vercel.app',
+  icons: ['https://avatars.githubusercontent.com/u/37784886'],
+};
 
 // Create wagmiConfig
-const chains = [mainnet, sepolia] as const
+const chains = [mainnet, sepolia] as const;
 export const config = defaultWagmiConfig({
   chains,
   projectId,
   metadata,
   ssr: true,
   storage: createStorage({
-    storage: cookieStorage
+    storage: cookieStorage,
   }),
-})
+});
